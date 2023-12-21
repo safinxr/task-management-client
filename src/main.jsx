8c829420f7d032e3abd0d2900ebbe0ad66e72c92
safinxr <safinxr@gmail.com>
2023-12-21 18:59:45 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Signin from './Pages/Signin/Signin';
+import Signup from './Pages/Signup/Signup';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/signin",
+    element: <Signin></Signin>,
+  },
+  {
+    path: "/signup",
+    element: <Signup></Signup>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
